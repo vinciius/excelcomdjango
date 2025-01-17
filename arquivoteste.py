8b475eab8778361f078c11150637b40b91cf30d0
vinciius <vinicius.kullmann@gmail.com>
2025-01-17 17:16:07 -0300

    adicionar implementação de envio por email dos relatórios modelo.

--- arquivoteste.py
+++ arquivoteste.py
@@ -1,6 +1,6 @@
 # Importar a base de dados
 import pandas as pd
-from prettytable import PrettyTable
+import win32com.client as win32
 
 # Visualizar a base de dados
 tabela_vendas = pd.read_excel('Vendas.xlsx')
@@ -11,43 +11,23 @@ tabela_faturamento = tabela_vendas[['ID Loja', 'Valor Final']].groupby('ID Loja'
 # Quantidade de produtos vendidos por loja
 tabela_quantidade = tabela_vendas[['ID Loja', 'Quantidade']].groupby('ID Loja').sum()
 
-# Ticket médio por produto em cada loja
-tabela_ticket_medio = tabela_vendas['Valor Final'] / tabela_vendas['Quantidade']
+# Calcular o ticket médio por loja
+# Agrupar por ID Loja e calcular a média do Valor Final e da Quantidade
+media_por_loja = tabela_vendas.groupby('ID Loja').agg({'Valor Final': 'mean', 'Quantidade': 'mean'})
+# Calcular o ticket médio
+media_por_loja['Ticket Médio'] = media_por_loja['Valor Final'] / media_por_loja['Quantidade']
+
+# Criar tabelas em HTML usando pandas
+faturamento_html = tabela_faturamento.to_html(index=True, header=True, float_format='R$ {:.2f}'.format)
+quantidade_html = tabela_quantidade.to_html(index=True, header=True)
+
+# Usar apenas a coluna 'Ticket Médio' para a tabela final
+ticket_html = media_por_loja[['Ticket Médio']].to_html(index=True, header=True, float_format='R$ {:.2f}'.format)
 
 # Enviar um email com o relatório
-
-
-# Tabela personalizada de faturamento
-faturamento_table = PrettyTable()
-faturamento_table.field_names = ["ID Loja", "Valor Final"]
-for index, row in tabela_faturamento.iterrows():
-    faturamento_table.add_row([row.name, f"R$ {row['Valor Final']:.2f}"])
-
-# Tabela personalizada de quantidade
-quantidade_table = PrettyTable()
-quantidade_table.field_names = ["ID Loja", "Quantidade Vendida"]
-for index, row in tabela_quantidade.iterrows():
-    quantidade_table.add_row([row.name, row['Quantidade']])
-
-# Tabela personalizada de ticket médio
-ticket_table = PrettyTable()
-ticket_table.field_names = ["ID Loja", "Ticket Médio"]
-for index, row in tabela_ticket_medio.items():
-    ticket_table.add_row([index, f"R$ {row:.2f}"])
-
-# Imprimir as tabelas
-print("\n" + "="*40)
-print("Faturamento por Loja")
-print("="*40)
-print(faturamento_table)
-
-print("\n" + "="*40)
-print("Quantidade de Produtos Vendidos por Loja")
-print("="*40)
-print(quantidade_table)
-print("="*40)
-
-print("\n" + "="*40)
-print("Ticket Médio por Loja")
-print("="*40)
-print(ticket_table)
+outlook = win32.Dispatch('outlook.application')
+mail = outlook.CreateItem(0)
+mail.To = 'bluecrushangel@outlook.com'
+mail.Subject = 'Relatório de vendas por loja'
+mail.HTMLBody = '''<h2> Relatório de vendas por loja </h2>''' + faturamento_html + '''<br>''' + quantidade_html + '''<br>''' + ticket_html
+mail.Send()
